309900fa70ca3661f73f2161c8eb5a668cf7adf1
mh-anwar <mohammad.anwar2939@gmail.com>
2024-09-13 03:38:23 -0400

    feat: complete ES6, MongoDB migration

--- backend/api/leaderboard/LeaderboardRouter.js
+++ backend/api/leaderboard/LeaderboardRouter.js
@@ -1,111 +1,110 @@
-import express from 'express';
-import Leaderboard from './LeaderboardSchema';
-import { getUserData } from '../user/UserRouter';
+import { Router } from 'express';
+import Leaderboard from './LeaderboardSchema.js';
+import { getUserData } from '../user/UserRouter.js';
+import User from '../user/UserSchema.js';
 
-const LeaderboardRouter = express.router();
+const LeaderboardRouter = Router();
 
 // /api/leaderboard/
 
 LeaderboardRouter.post('/userGames', async (req, res) => {
-	const body = req.body;
-	const user = await getUserData(data.name);
-	if (user.passKey === body.passKey) {
-		res.send({ data: user.games });
-	} else {
-		res.send({ data: null });
-	}
+  const body = req.body;
+  const user = await getUserData(data.name);
+  if (user.passKey === body.passKey) {
+    res.send({ data: user.games });
+  } else {
+    res.send({ data: null });
+  }
 });
 
 LeaderboardRouter.post('/gamePlay', async (req, res) => {
-	const data = req.body;
-	const name = data.name;
-	const gameType = data.gameType;
-	const gameResult = data.gameResult;
-	const gameDate = data.gameDate;
-	let actualTime = new Date(gameDate).toString().split(' ');
-	actualTime = actualTime[4];
-	let fullOppName = data.opponent;
-	fullOppName = fullOppName.split(',');
-	const oppName = fullOppName[0] + ' ' + fullOppName[1];
+  const data = req.body;
+  const name = data.name;
+  const gameType = data.gameType;
+  const gameResult = data.gameResult;
+  const gameDate = data.gameDate;
+  let actualTime = new Date(gameDate).toString().split(' ')[4];
 
-	const user = await getUserData(name);
-	let userGames = user.games;
-	const oppData = await getUserData(oppName);
-	let oppGames = oppData.games;
-	let userWins, oppLosses, userKey, oppKey;
+  let fullOppName = data.opponent.split(',');
+  const oppName = fullOppName[0] + ' ' + fullOppName[1];
 
-	if (user.passKey === data.passKey) {
-		// Update USERDB with games
-		// WINNER INPUTS INTO SYSTEM
-		let newUserData = {
-			date: gameDate,
-			opponent: oppName,
-			result: gameResult,
-			type: gameType,
-			confirmed: true,
-		};
-		userGames[actualTime] = newUserData;
-		const userUpdates = {
-			games: userGames,
-		};
-		await userDB.update(userUpdates, userName);
+  const user = await getUserData(name);
+  let userGames = user.games;
+  const oppData = await getUserData(oppName);
+  let oppGames = oppData.games;
+  let userWins, oppLosses;
 
-		let oppResult = gameResult;
-		if (gameResult === 'won') {
-			oppResult = 'lost';
-		}
-		let newOppData = {
-			date: gameDate,
-			opponent: userName,
-			result: oppResult,
-			type: gameType,
-			confirmed: false,
-		};
-		oppGames[actualTime] = newOppData;
-		const oppUpdates = {
-			games: oppGames,
-		};
-		await userDB.update(oppUpdates, oppName);
-		// Update leaderboard to increase wing
-		if (gameResult === 'won') {
-			let boardData = await boardDB.fetch();
-			for (let i = 0; i < boardData.items.length; i++) {
-				if (boardData.items[i]['name'] === userName) {
-					userWins = boardData.items[i]['won'];
-					userKey = boardData.items[i]['key'];
-				} else if (boardData.items[i]['name'] === oppName) {
-					oppLosses = boardData.items[i]['lost'];
-					oppKey = boardData.items[i]['key'];
-				}
-			}
-			const userBoardUpdate = {
-				won: userWins + 1,
-			};
-			await boardDB.update(userBoardUpdate, userKey);
-			const oppBoardUpdate = {
-				lost: oppLosses + 1,
-			};
-			await boardDB.update(oppBoardUpdate, oppKey);
-		}
-		res.send({ success: 'Leaderboard updated' });
-	} else {
-		res.send({ failed: 'Failed. Your login is invalid.' });
-	}
+  if (user.passKey === data.passKey) {
+    // Update USERDB with games
+    // WINNER INPUTS INTO SYSTEM
+    let newUserData = {
+      date: gameDate,
+      opponent: oppName,
+      result: gameResult,
+      type: gameType,
+      confirmed: true,
+    };
+    userGames[actualTime] = newUserData;
+    const userGamesUpdate = {
+      games: userGames,
+    };
+    await User.findOneAndUpdate({ name: name }, userGamesUpdate);
+
+    let oppResult = gameResult === 'won' ? 'lost' : gameResult;
+    let newOppData = {
+      date: gameDate,
+      opponent: name,
+      result: oppResult,
+      type: gameType,
+      confirmed: false,
+    };
+    oppGames[actualTime] = newOppData;
+    const oppGameUpdate = {
+      games: oppGames,
+    };
+    await User.findOneAndUpdate({ name: oppName }, oppGameUpdate);
+
+    // Update leaderboard to increase wins and losses
+    if (gameResult === 'won') {
+      const userLeaderboard = getUserLeaderBoard(name);
+      userWins = userLeaderboard.won;
+
+      const oppLeaderboard = getUserLeaderBoard(oppName);
+      oppLosses = oppLeaderboard.won;
+
+      await Leaderboard.findOneAndUpdate(
+        { name: name },
+        {
+          won: userWins + 1,
+        }
+      );
+
+      await Leaderboard.findOneAndUpdate(
+        { name: oppName },
+        {
+          lost: oppLosses + 1,
+        }
+      );
+    }
+    res.send({ success: 'Leaderboard updated' });
+  } else {
+    res.send({ failed: 'Failed. Your login is invalid.' });
+  }
 });
 async function createLeaderBoardEntry(lost, won, name, rank) {
-	const newLeaderBoardEntry = new Leaderboard({
-		lost: 1,
-		won: 1,
-		name: name,
-		move: 0,
-		rank: Leaderboard.countDocuments({}) + 1,
-	});
+  const newLeaderBoardEntry = new Leaderboard({
+    lost: 1,
+    won: 1,
+    name: name,
+    move: 0,
+    rank: Leaderboard.countDocuments({}) + 1,
+  });
 
-	await newLeaderBoardEntry.save();
+  await newLeaderBoardEntry.save();
 }
 
 async function getUserLeaderBoard(name) {
-	const user = await Leaderboard.findOne({ name: name });
-	return user;
+  const user = await Leaderboard.findOne({ name: name });
+  return user;
 }
 export { LeaderboardRouter, createLeaderBoardEntry };
--- backend/api/user/UserRouter.js
+++ backend/api/user/UserRouter.js
@@ -1,97 +1,95 @@
-import express from 'express';
-import User from './UserSchema';
-import { createLeaderBoardEntry } from '../leaderboard/LeaderboardRouter';
-const crypto = require('crypto');
+import { Router } from 'express';
+import User from './UserSchema.js';
+import { createLeaderBoardEntry } from '../leaderboard/LeaderboardRouter.js';
 
-const UserRouter = express.router();
+const UserRouter = Router();
 
 // /api/user/
-UserRouter.post('/', async (req, res) => {});
 
 UserRouter.get('/:name', async (req, res) => {
-	const name = req.body.name;
-	const userPasskey = await checkUser(name);
-	res.send({ success: true, passKey: userPasskey });
+  const name = req.body.name;
+  const userPasskey = await checkUser(name);
+  res.send({ success: true, passKey: userPasskey });
 });
 
 UserRouter.post('/login', async (request, res) => {
-	const body = req.body;
-	let userAuthentication = await authenticateUser(body.name, body.password);
+  const body = req.body;
+  let userAuthentication = await authenticateUser(body.name, body.password);
 
-	if (userAuthentication !== false && userAuthentication !== undefined) {
-		res.send({ success: true, passKey: userAuthentication });
-	} else if (userAuthentication === undefined) {
-		res.send({ success: "User doesn't exist" });
-	} else {
-		res.send({ success: 'Incorrect password' });
-	}
+  if (userAuthentication !== false && userAuthentication !== undefined) {
+    res.send({ success: true, passKey: userAuthentication });
+  } else if (userAuthentication === undefined) {
+    res.send({ success: "User doesn't exist" });
+  } else {
+    res.send({ success: 'Incorrect password' });
+  }
 });
 
 UserRouter.post('/join', async (req, res) => {
-	const body = req.body;
-	let name = body.name;
-	let email = body.email;
-	let password = body.password;
-	let color = body.color;
+  const body = req.body;
+  let name = body.name;
+  let email = body.email;
+  let password = body.password;
+  let color = body.color;
 
-	const userPasskey = await checkUser(name);
+  const userPasskey = await checkUser(name);
 
-	if (email.includes('@ddsbstudent.ca')) {
-		if (userPasskey === false) {
-			let passKey = await createUser(name, email, password, color);
-			res.send({ success: true, passKey: passKey });
-		} else {
-			res.send({ success: true, passKey: userPasskey });
-		}
-	} else {
-		res.send({ success: 'Use your student email.' });
-	}
+  if (email.includes('@ddsbstudent.ca')) {
+    if (userPasskey === false) {
+      let passKey = await createUser(name, email, password, color);
+      res.send({ success: true, passKey: passKey });
+    } else {
+      res.send({ success: true, passKey: userPasskey });
+    }
+  } else {
+    res.send({ success: 'Use your student email.' });
+  }
 });
 
 async function createUser(name, email, password, color) {
-	var passKey = crypto.randomBytes(12).toString('hex');
-	const newUser = new User({
-		name: name,
-		games: {},
-		password: password,
-		email: email,
-		passKey: passKey,
-		color: color,
-	});
-	await newUser.save();
-	createLeaderBoardEntry(1, 1, name, 0);
-	return passKey;
+  var passKey = crypto.randomBytes(12).toString('hex');
+  const newUser = new User({
+    name: name,
+    games: {},
+    password: password,
+    email: email,
+    passKey: passKey,
+    color: color,
+  });
+  await newUser.save();
+  createLeaderBoardEntry(1, 1, name, 0);
+  return passKey;
 }
 
 async function authenticateUser(name, password) {
-	const user = await User.findOne({
-		name: name,
-	});
-	if (user) {
-		if (user.password === password) {
-			return user.passKey;
-		} else {
-			return false;
-		}
-	} else {
-		return undefined;
-	}
+  const user = await User.findOne({
+    name: name,
+  });
+  if (user) {
+    if (user.password === password) {
+      return user.passKey;
+    } else {
+      return false;
+    }
+  } else {
+    return undefined;
+  }
 }
 
 async function checkUser(name) {
-	const user = getUserData(name);
-	if (user) {
-		return user.passKey;
-	} else {
-		return false;
-	}
+  const user = getUserData(name);
+  if (user) {
+    return user.passKey;
+  } else {
+    return false;
+  }
 }
 
 async function getUserData(name) {
-	const user = await User.findOne({
-		name: name,
-	});
-	return user;
+  const user = await User.findOne({
+    name: name,
+  });
+  return user;
 }
 
 export { UserRouter, getUserData };
