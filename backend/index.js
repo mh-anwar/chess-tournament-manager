4bfd1eb635308bde903daafc73821c05d59b902b
mh-anwar <mohammad.anwar2939@gmail.com>
2024-09-13 01:31:05 -0400

    Begin migration from Deta to Render

--- backend/index.js
+++ backend/index.js
@@ -1,199 +1,37 @@
+import mongoose from 'mongoose';
 const express = require('express');
 const cors = require('cors');
-const shrinkRay = require('shrink-ray-current');
 const app = express();
 const path = require('path');
-const base64 = require('base-64');
-const { Deta } = require('deta');
-const crypto = require('crypto');
 
-const deta = Deta(process.env.DETA_PROJECT_KEY);
-const userDB = deta.Base('users');
-const boardDB = deta.Base('leaderboard');
+import { LeaderboardRouter } from './api/leaderboard/LeaderboardRouter';
+import { UserRouter } from './api/user/UserRouter';
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.listen(process.env.PORT);
 app.use(cors());
-app.use(shrinkRay());
 app.use(express.static(path.resolve(__dirname, './deploy')));
 
-app.post('/authjoin', async (request, res) => {
-	let name = request.body.name;
-	let email = request.body.email;
-	let password = request.body.password;
-	let color = request.body.color;
-
-	let userExists = await checkUser(name);
-	await userExists;
-	if (email.includes('@ddsbstudent.ca')) {
-		if (userExists === false) {
-			let passKey = await createUser(name, email, password, color);
-			res.send({ success: true, passKey: passKey });
-		} else {
-		}
-	} else {
-		res.send({ success: 'Use your student email.' });
-	}
-});
-
-async function createUser(userName, email, password, color) {
-	var passKey = crypto.randomBytes(12).toString('hex');
-
-	await userDB.put(
-		{
-			games: {},
-			password: password,
-			email: email,
-			passKey: passKey,
-			color: color,
-		},
-		userName
-	);
-
-	const leaderboard = await boardDB.fetch();
-	await boardDB.put({
-		lost: 1,
-		won: 1,
-		name: userName,
-		move: 0,
-		rank: leaderboard.count + 1,
+try {
+	await mongoose.connect('<URL>', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
 	});
-	return passKey;
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, './deploy', 'index.html'));
+	});
+	app.get('/favicon.ico', async (req, res) => {
+		res.sendFile(path.resolve(__dirname, './deploy', 'favicon.ico'));
+	});
+} catch (error) {
+	console.log(error);
+	process.exit(1);
 }
 
-async function checkUser(userName) {
-	const user = await userDB.get(userName);
-	if (user) {
-		return user.passKey;
-	} else {
-		return false;
-	}
-}
-
-async function authenticateUser(userName, password) {
-	let user = await userDB.get(userName);
-	if (user) {
-		if (user.password === password) {
-			return user.passKey;
-		} else {
-			return false;
-		}
-	} else {
-		return undefined;
-	}
-}
-
-app.post('/authlogin', async (request, res) => {
-	await request;
-	let name = request.body.name;
-	let password = request.body.password;
-	let userAuthentication = await authenticateUser(name, password);
-
-	if (userAuthentication !== false && userAuthentication !== undefined) {
-		res.send({ success: true, passKey: userAuthentication });
-	} else if (userAuthentication === undefined) {
-		res.send({ success: "User doesn't exist" });
-	} else {
-		res.send({ success: 'Incorrect password' });
-	}
-});
-
-app.post('/userGames', async (request, res) => {
-	await request;
-	const data = request.body;
-	const user = await userDB.get(data.name);
-	if (user.passKey === data.passKey) {
-		res.send({ data: user.games });
-	} else {
-		res.send({ data: null });
-	}
-});
-
-app.post('/gamePlay', async (request, res) => {
-	await request;
-	const data = request.body;
-	const userName = base64.decode(data.name);
-	const gameType = base64.decode(data.gameType);
-	const gameResult = base64.decode(data.gameResult);
-	const gameDate = data.gameDate;
-	let actualTime = new Date(gameDate).toString().split(' ');
-	actualTime = actualTime[4];
-	let fullOppName = base64.decode(data.opponent);
-	fullOppName = fullOppName.split(',');
-	const oppName = fullOppName[0] + ' ' + fullOppName[1];
-	const userData = await userDB.get(userName);
-	let userGames = userData.games;
-	const oppData = await userDB.get(oppName);
-	let oppGames = oppData.games;
-	let userWins, oppLosses, userKey, oppKey;
-
-	if (userData.passKey === base64.decode(data.passKey)) {
-		// Update USERDB with games
-		// WINNER INPUTS INTO SYSTEM
-		let newUserData = {
-			date: gameDate,
-			opponent: oppName,
-			result: gameResult,
-			type: gameType,
-			confirmed: true,
-		};
-		userGames[actualTime] = newUserData;
-		const userUpdates = {
-			games: userGames,
-		};
-		await userDB.update(userUpdates, userName);
-
-		let oppResult = gameResult;
-		if (gameResult === 'won') {
-			oppResult = 'lost';
-		}
-		let newOppData = {
-			date: gameDate,
-			opponent: userName,
-			result: oppResult,
-			type: gameType,
-			confirmed: false,
-		};
-		oppGames[actualTime] = newOppData;
-		const oppUpdates = {
-			games: oppGames,
-		};
-		await userDB.update(oppUpdates, oppName);
-		// Update leaderboard to increase wing
-		if (gameResult === 'won') {
-			let boardData = await boardDB.fetch();
-			for (let i = 0; i < boardData.items.length; i++) {
-				if (boardData.items[i]['name'] === userName) {
-					userWins = boardData.items[i]['won'];
-					userKey = boardData.items[i]['key'];
-				} else if (boardData.items[i]['name'] === oppName) {
-					oppLosses = boardData.items[i]['lost'];
-					oppKey = boardData.items[i]['key'];
-				}
-			}
-			const userBoardUpdate = {
-				won: userWins + 1,
-			};
-			await boardDB.update(userBoardUpdate, userKey);
-			const oppBoardUpdate = {
-				lost: oppLosses + 1,
-			};
-			await boardDB.update(oppBoardUpdate, oppKey);
-		}
-		res.send({ success: 'Leaderboard updated' });
-	} else {
-		res.send({ failed: 'Failed. Your login is invalid.' });
-	}
-});
-
-// In order to deploy a /deploy folder with an index.html will be necessary
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, './deploy', 'index.html'));
-});
-app.get('/favicon.ico', async (req, res) => {
-	res.sendFile(path.resolve(__dirname, './deploy', 'favicon.ico'));
-});
+app.use('/api/user', UserRouter);
+app.use('/api/leaderboard', LeaderboardRouter);
 
 // userDB example
 /* userDB.put(
