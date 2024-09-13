640e74278f423ea774b33c317e8b0128172484da
mh-anwar <mohammad.anwar2939@gmail.com>
2024-09-13 03:39:20 -0400

    refactor: update code to match general repository

--- backend/index.js
+++ backend/index.js
@@ -1,48 +1,47 @@
 import mongoose from 'mongoose';
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const path = require('path');
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { UserRouter } from './api/user/UserRouter.js';
+import { LeaderboardRouter } from './api/leaderboard/LeaderboardRouter.js';
 
-import { LeaderboardRouter } from './api/leaderboard/LeaderboardRouter';
-import { UserRouter } from './api/user/UserRouter';
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.listen(process.env.PORT);
 app.use(cors());
-app.use(express.static(path.resolve(__dirname, './deploy')));
+app.use(express.static(path.resolve(__dirname, './dist')));
 
-try {
-	await mongoose.connect('<URL>', {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
+const spinUpServer = async () => {
+  console.log(process.env.MONGO_DB);
+  try {
+    await mongoose.connect(process.env.MONGO_DB);
 
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, './deploy', 'index.html'));
-	});
-	app.get('/favicon.ico', async (req, res) => {
-		res.sendFile(path.resolve(__dirname, './deploy', 'favicon.ico'));
-	});
-} catch (error) {
-	console.log(error);
-	process.exit(1);
-}
-
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, './dist', 'index.html'));
+    });
+    app.get('/favicon.ico', async (req, res) => {
+      res.sendFile(path.resolve(__dirname, './dist', 'favicon.ico'));
+    });
+    // Start the server after DB connection
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+};
 app.use('/api/user', UserRouter);
 app.use('/api/leaderboard', LeaderboardRouter);
-
-// userDB example
-/* userDB.put(
-  {
-    games: { John: 'lost', Doe: 'lost', Jane: 'won' },
-    password: 'test',
-    email: 'email@gmail.com',
-  },
-  'Jack'
-); */
-
+spinUpServer();
 // TODO Archive old unused code
 /*
 // Was used when user avatar called on backend, pretty unnecessary
